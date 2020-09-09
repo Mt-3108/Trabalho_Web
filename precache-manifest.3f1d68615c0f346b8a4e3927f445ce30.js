28dcf53ffc08201fbd7308edf672e44f6aff0aa6
Jubilada <mateus_cp2013@hotmail.com>
2020-09-09 18:20:55 -0300

    Updates

---
+++ precache-manifest.3f1d68615c0f346b8a4e3927f445ce30.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3754fcdf00bff47269005587b15daa13",
+    "url": "/Trabalho_Web/index.html"
+  },
+  {
+    "revision": "c62d79268d34787818fa",
+    "url": "/Trabalho_Web/static/css/main.06a7ef02.chunk.css"
+  },
+  {
+    "revision": "3b070f085414969f80c6",
+    "url": "/Trabalho_Web/static/js/2.5f671702.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Trabalho_Web/static/js/2.5f671702.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c62d79268d34787818fa",
+    "url": "/Trabalho_Web/static/js/main.582e6d6a.chunk.js"
+  },
+  {
+    "revision": "dea28db21923972e2ad34e2950e4f628",
+    "url": "/Trabalho_Web/static/js/main.582e6d6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "906b03bf00712668bd1f",
+    "url": "/Trabalho_Web/static/js/runtime-main.c1995df1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Trabalho_Web/static/media/logo.5d5d9eef.svg"
+  }
+]);
